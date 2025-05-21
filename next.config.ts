86f2a0a86f4b2de8dc097fa7e24fee51afc1ab68
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2025-05-21 00:00:22 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   devIndicators: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
